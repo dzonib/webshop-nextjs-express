b5d97b194b9205e6cb0c46d7070a801498742853
dzonib <nikolabosnjak381@gmail.com>
2019-03-18 07:55:54 +0100

    adding validation

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,48 @@
 const express = require("express")
+const { check, validationResult } = require("express-validator/check")
 
 const User = require("../db/models/User")
 const router = express.Router()
 
-router.post("/register", async (req, res) => {
-    const { name, email, password, address } = req.body
-    try {
-        const user = await User.findOne({ where: { email } })
-        if (user) throw new Error('Korisnik sa tim emejlom je već registrovan.')
+router.post(
+    "/register",
+    [
+        check("name", "Name must be at lest 2 charachters long")
+            .trim()
+            .isLength({ min: 2 }),
+        check("password", "Password must be at least 5 characthers long")
+            .trim()
+            .isLength({ min: 5 }),
+        check("email", "Please enter valid email")
+            .trim()
+            .isEmail()
+    ],
+    async (req, res) => {
+        const { name, email, password, address } = req.body
+        try {
+            const user = await User.findOne({ where: { email } })
 
-        const newUser = await User.create({
-            name,
-            address,
-            email,
-            password
-        })
+            const errors = validationResult(req)
 
-        res.json(newUser)
-    } catch (e) {
-        console.log(e.message)
+            if (!errors.isEmpty()) {
+                
+                return res.status(403).json(errors.mapped())
+            }
+            if (user)
+                throw new Error("Korisnik sa tim emejlom je već registrovan.")
+
+            const newUser = await User.create({
+                name,
+                address,
+                email,
+                password
+            })
+
+            res.json(newUser)
+        } catch (e) {
+            console.log(e.message)
+        }
     }
-})
+)
 
 module.exports = router
